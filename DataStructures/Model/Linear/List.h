66d16dca2ab399a2b8dd85cd8196a958320c624f
Bryson Spaulding <brysonian10@hotmail.com>
2019-02-07 08:25:40 -0700

    created a new folder and headfile called LIsts.

---
+++ DataStructures/Model/Linear/List.h
@@ -0,0 +1,14 @@
+//
+//  List.h
+//  DataStructures
+//
+//  Created by Spaulding, Bryson on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+
+
+
+#endif /* List_h */
