0c2fe84bfcae0e7c4c470e7c0b48ed4fa229e125
Bryson Spaulding <brysonian10@hotmail.com>
2019-03-06 09:50:14 -0700

    Created the Graph header file and started to work on it.

---
+++ DataStructures/Model/Linear/Graph.hpp
@@ -0,0 +1,47 @@
+//
+//  Graph.h
+//  DataStructures
+//
+//  Created by Spaulding, Bryson on 3/6/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 50;
+    bool adjecencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertecees[]);
+    
+public:
+    Graph();
+    //Graph operations
+    
+    void addVertex(const Type& value);
+    
+    //Connect vertices
+    void addEdge(int source, int target);
+    void addEdgeUndireced(int source, int target);
+    void addEdgeCost(int source, int tareger, int cost);
+    //Disconnect vertices
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    
+}
+
+
+#endif /* Graph_h */
