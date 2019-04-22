2d2bd985f3a44eb036a0eb4b5fdd2c3ae31a1f4b
Bryson Spaulding <brysonian10@hotmail.com>
2019-04-22 08:49:37 -0600

    Finish the tester and then added more methods to the BinarySearchTree.

--- DataStructures/Model/Linear/BinarySearchTree.hpp
+++ DataStructures/Model/Linear/BinarySearchTree.hpp
@@ -68,18 +68,76 @@ int BinarySearchTree<Type> :: getHeight()
 template <class Type>
 int BinarySearchTree<Type> :: getSize()
 {
-    return -1;
+    int size = 0;
+    size += calculatedSize(this->root);
+    
+    return size;
+}
+
+template <class Type>
+int BinarySearchTree<Type> :: calculatedSize(BinaryTreeNode<Type> * current)
+{
+    if(current != nullptr)
+    {
+        return calculatedSize(current->getLeftChild()) + calculatedSize(current->getRightChild()) + 1;
+    }
+    return 0;
 }
 
 template <class Type>
 bool BinarySearchTree<Type> :: isComplete()
 {
-    return false;
+    int index = 0;
+    int size = getSize();
+    
+    return isComplete(this->root, index, size);
+}
+
+template <class Type>
+bool BinarySearchTree<Type> :: isComplete(BinaryTreeNode<Type> * startNode, int index, int size)
+{
+    if(startNode == nullptr)
+    {
+        return true;
+    }
+    
+    if(index >= size)
+    {
+        return false;
+    }
+    
+    return (isComplete(startNode->getLeftChild(), 2 = index + 1, size) && isComplete(startNode->getRightChild(), 2 * index + 2, size));
 }
 
 template <class Type>
 bool BinarySearchTree<Type> :: isBalanced()
 {
+    return isBalanced(this->root);
+}
+
+template <class Type>
+bool BinarySearchTree<Type> :: isBalanced(BinaryTreeNode<Type> * current)
+{
+    int leftHeight = 0;
+    int rightHeight = 0;
+    
+    if(current == nullptr)
+    {
+        return true;
+    }
+    
+    leftHeight = calculatedHeight(current->getLeftChild());
+    rightHeight = calculatedHeight(current->getRightChild());
+    
+    int heightDifference = abs(leftHeight) - rightHeight);
+    bool leftBalanced = isBalanced(current->getLeftChild());
+    bool rightBalanced = isBalanced(current->getRightChild());
+    
+    if (heightDifference <= 1 && leftBalanced && rightBalanced)
+    {
+        return true;
+    }
+    
     return false;
 }
 
