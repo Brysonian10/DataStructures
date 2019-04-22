28d6909d5f6bf1afb0b6dbb49259f27dff538e05
Bryson Spaulding <brysonian10@hotmail.com>
2019-04-22 08:05:15 -0600

    BinaryTreeTester was created.

---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,23 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Spaulding, Bryson on 4/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+#include "../Model/Linear/BinarySearchTree.hpp"
+#include <iostream>
+
+class BinaryTree Tester
+{
+    BinaryTreeNode<int> testNode;
+    BinarySearchTree<int> testTree;
+}
+
+
+#endif /* BinaryTreeTester_hpp */
